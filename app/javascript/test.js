90c50045b4d967b8bd36341e3e9400c80aa76990
nicholastonelli <nicholasevantonelli@gmail.com>
2025-09-15 11:09:52 -0400

    stable

---
+++ app/javascript/test.js
@@ -0,0 +1,11 @@
+//import { Graphiql } from "graphiql"
+
+//import 'graphiql/graphiql.css'
+//import { createGraphiQLFetcher } from '@graphiql/toolkit'
+
+import { createConsumer } from "@rails/actioncable"
+//import createActionCableFetcher from 'graphql-ruby-client/subscriptions/createActionCableFetcher'
+
+//import 'graphql-ruby-client/subscriptions/createActionCableFetcher'
+
+console.log('app javascript connected')
